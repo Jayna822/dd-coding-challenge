f79fc4640c758e9107bf658ba82cf2db7c71f693
Jayna <jayna@jaynas-mbp.attlocal.net>
2019-08-09 17:45:40 -0500

    Added merge route

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,14 @@ def health_check():
     """
     app.logger.info("Health Check!")
     return Response("All Good!", status=200)
+
+
+@app.route("/merge/<profile>", methods=["GET"])
+@app.route("/merge/<profile>/<profile2>", methods=["GET"])
+def merge(profile, profile2=None):
+    """
+    Merge profiles from Github and Bitbucket together.
+    In the case of differing profile names,
+        the Github profile name must be first followed by the Bitbucket profile name
+    """
+    return 'Profile1: {}, Profile2: {}'.format(profile, profile2)

1d2f1fa3055730414fde51d50776df873b71c440
Jayna <jayna@jaynas-mbp.attlocal.net>
2019-08-10 10:01:37 -0500

    added logic to parse out data from bitbucket api response

--- app/api_logic.py
+++ app/api_logic.py
@@ -1,4 +1,5 @@
 import requests
+import copy
 
 GITHUB_API = 'https://api.github.com' # currently v3
 BITBUCKET_API = 'https://api.bitbucket.org/2.0'
@@ -26,7 +27,7 @@ REPO_DETAILS_TEMPLATE = {
 def get_github_info(org):
     response = requests.get('{}/orgs/{}/repos'.format(GITHUB_API, org))
     if response.ok:
-        results = RESULTS_TEMPLATE.copy()
+        results = copy.deepcopy(RESULTS_TEMPLATE)
         repos = response.json()
         for repo in repos:
             if repo.get('fork'):
@@ -61,10 +62,55 @@ def get_github_info(org):
         results['total_topics'] = len(results['topics'].keys())
 
         return results
+    else:
+        # Response not ok
+        return 'Error'
 
 
 def get_bitbucket_info(team):
-    res = requests.get('{}/repositories/{}'.format(BITBUCKET_API, team))
-    if res.ok:
-        data = res.json()
-        print('break')
+    results = copy.deepcopy(RESULTS_TEMPLATE)
+
+    response = requests.get('{}/repositories/{}'.format(BITBUCKET_API, team))
+    if response.ok:
+        data = response.json()
+        repos = data.get('values')
+        if repos:
+            for repo in repos:
+                repo_details = REPO_DETAILS_TEMPLATE.copy()
+                repo_details['name'] = repo.get('name')
+
+                # api response doesn't seem to contain fork v original information
+                results['total_original_repos'] += 1
+
+                # language info
+                if repo.get('language'):
+                    repo_details['language'] = repo.get('language')
+                    if results['languages'].get(repo.get('language')):
+                        results['languages'][repo.get('language')] += 1
+                    else:
+                        results['languages'][repo.get('language')] = 1
+
+                # watchers info
+                watchers_link = repo['links'].get('watchers')
+                if watchers_link:
+                    watchers_response = requests.get(watchers_link['href'])
+                    if watchers_response.ok:
+                        watchers_data = watchers_response.json()
+                        results['total_watchers'] += watchers_data.get('size')
+                        repo_details['number_of_watchers'] = watchers_data.get('size')
+
+                # topics info
+                # I don't see anything about topics or an equivalent
+
+                results['repo_details'].append(repo_details)
+
+        results['total_languages'] = len(results['languages'].keys())
+    else:
+        # Response not ok
+        return 'Error'
+
+    # There's another api call to get a team's followers. This number is different from the total
+    #   watchers count I got above. I assume followers are team-level, while watchers are repo-level.
+    #   Going to stick with returning watchers since it seems more similar to Github watchers.
+    #   response = requests.get('{}/teams/{}/followers'.format(BITBUCKET_API, team))
+
